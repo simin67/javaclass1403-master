15ef94466a8c69b1ffa4c8b665d8732902f88867
s.morovati <simin.morovatii@gmail.com>
2024-05-28 12:56:21 +0330

    simin information Data.

--- Main.java
+++ Main.java
@@ -1,8 +1,19 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello word");
-        System.out.println("hello word2");
-        System.out.println("hello world 3");
+        String name = "simin";
+        String family = "morovati";
+        int age = 36;
+        boolean married = true;
+        float avg = 17.56f;
+        float height = 156.2f;
+        String gender = "female";
+
+        System.out.println("name: " + name);
+        System.out.println("family: " + family);
+        System.out.println("age: " + age);
+        System.out.println("height: " + height);
+        System.out.println("married: " + married);
+        System.out.println("gender: " + gender);
     }
 }
