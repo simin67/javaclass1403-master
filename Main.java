44339c71edbce0f15752500b92de1683eb4549a4
s.morovati <simin.morovatii@gmail.com>
2024-05-28 12:52:10 +0330

    Test Git Master

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello word");
         System.out.println("hello word2");
+        System.out.println("hello world 3");
     }
 }
