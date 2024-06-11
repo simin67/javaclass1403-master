9be49d7c0f1f071871821eff3a0ef8e23942e3e7
s.morovati <simin.morovatii@gmail.com>
2024-06-11 11:10:19 +0330

    tamrin3-edit

--- src/session3/Main2.java
+++ src/session3/Main2.java
@@ -4,7 +4,7 @@ public class Main2 {
 
 
         public static void main(String[] args) {
-            String str3=String.format("My name is %s and my age is %s","simin",35);
+            String str3=String.format("My name is %s and my age is %s","simin",36);
             String str4= str3.toUpperCase();
             String str5=str4.replace("SIMIN","Fanap");
             String str6=str5.substring(0,str5.indexOf("Fanap") +"Fanap".length());
